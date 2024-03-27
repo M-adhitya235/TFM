4e89a5f809aa54852cb2db1a437808b774d46a14
Andin <11211003@student.itk.ac.id>
2024-03-28 00:12:50 +0800

    Wishlist new

---
+++ src/components/CC.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const CC = ({ product, onAddToCart }) => {
+  const { image, title, harga, status } = product;
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
+
+  const addToCart = () => {
+    setIsAddedToCart(true);
+    onAddToCart(product);
+  };
+
+  return (
+    <div className="bg-white p-6 rounded-lg flex flex-col justify-center items-center">
+      <div className="w-40 h-40 mb-4">
+        <img
+          src={image}
+          alt={title}
+          className="object-cover w-full h-full rounded-md"
+        />
+      </div>
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-gray-700 text-left mb-1 ">Harga: {harga}</p>
+      <p className="text-gray-700 text-left mb-1">Status: {status}</p>
+      <div className="flex">
+        {!isAddedToCart ? (
+          <button
+            className="bg-white hover:bg-gray-100 text-black font-bold py-2 px-4 rounded-r border border-black"
+            onClick={addToCart}
+          >
+            Add to Cart
+          </button>
+        ) : (
+          <p className="text-green-500 mt-4 mx-2">Added to Cart</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CC;
--- src/pages/Wishlist/index.jsx
+++ src/pages/Wishlist/index.jsx
@@ -1,55 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import CC from "../../components/CC";
 
-const Wishlist = () => {
-  const [wishlist, setWishlist] = useState([]);
+function Wishlist() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
-  const addToWishlist = (product) => {
-    setWishlist([...wishlist, product]);
-  };
-
-  const removeFromWishlist = (index) => {
-    const newWishlist = [...wishlist];
-    newWishlist.splice(index, 1);
-    setWishlist(newWishlist);
-  };
+  useEffect(() => {
+    fetch('/pro.json')
+      .then(response => {
+        if (!response.ok) { 
+          throw new Error('Gagal mengambil data produk');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(error => {
+        setError(error.message);
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className="min-h-screen bg-white-200">
-      <Navbar />
+      <Navbar isDashboard={true} />
       <div className="container mx-auto px-4 py-8">
-      <h1 className="text-xl text-gray-800 font-bold mb-4 ml-4 relative">
-          <span className="absolute text-black px-2 py-1" style={{ right: "-2.5rem", top: "0.3rem", fontSize: "1.0rem",
-              boxShadow: "0 0 0 2px black"}}>
-            Move ALL to Cart
-          </span>
-          Wishlist
-        </h1>
+        <h1 className="text-xl text-gray-800 font-bold mb-4 ml-4">Wish List</h1>
+        {/* Menampilkan pesan kesalahan jika terjadi */}
+        {error && <p className="text-red-500">{error}</p>}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Card List */}
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
+          {/* Merender kartu untuk setiap produk */}
+          {products.map(P => (
+            <CC
+              key={P.id}
+              product={P}
+              style={{ marginBottom: "1.5rem" }} // Menambahkan jarak antara kartu-kartu produk
+            />
+          ))}
         </div>
       </div>
-      <div className="container mx-auto px-4 py-8">
-      <h1 className="text-xl text-gray-800 font-bold mb-4 ml-4 relative">
-      <Link to="/" 
-            className="absolute text-black px-2 py-1"
-            style={{right: "-2.5rem", top: "0.3rem", fontSize: "1.0rem", boxShadow: "0 0 0 2px black", textDecoration: "none", cursor: "pointer"}}>
-            See ALL
-          </Link>
-          Just For YOU
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Card List */}
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
-          <CardComponent imageUrl="https://www.koreandae.com/image-product/img3413-1605778321.jpg" title="SUPER JUNIOR - ALBUM VOL.9 [TIME SLIP]" description="Rp 295.000" style={{ marginTop: "2rem", marginbottom: "3rem" }} />
-        </div>
-      </div>
-      <Footer />
     </div>
   );
-};
+}
 
 export default Wishlist;
